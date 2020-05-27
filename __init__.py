3340317565c2598be9290f8bad6cb52f8809196c
Poenomaly <djbenson741@gmail.com>
2020-05-26 22:44:15 -0600

    Planned out more TODOs

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 
 from flask import Flask, Blueprint, jsonify
 from flask_sqlalchemy import SQLAlchemy
+# TODO: from routes.home_routes import home_routes
+# TODO: from routes.predict_routes import predict_routes
 
 # File path for database
 # TODO DATABASE_URI = "sqlite:////_________________
@@ -14,15 +16,15 @@ db = SQLAlchemy()
 app = Flask(__name__)
 
 
-# TODO def create_app():
-    # app = Flask(__name__)
-    # TODO app.config____________
-    # TODO db.init_app(app)
+def create_app():
+    app = Flask(__name__)
+    # TODO: app.config____________
+    # TODO: db.init_app(app)
 
-    # TODO app.register_blueprint(____routes)
-    # TODO app.register_blueprint(____routes)
+    # TODO: app.register_blueprint(home_routes)
+    # TODO: app.register_blueprint(predict_routes)
     
-    # return app
+    return app
 
 # Create home route
 @app.route("/")
@@ -32,11 +34,15 @@ def index():
 # Create predict route
 @app.route('/predict') #, methods=['POST'])
 def predict():
-    # TODO app.logger.info("/predict beginning...")
-    # TODO request_data = request.get_json(force=True)
-    # TODO input_text = request_data['text']
+    # TODO: app.logger.info("/predict beginning...")
+    # TODO: request_data = request.get_json(force=True)
+    # TODO: input_text = request_data['text']
+    # TODO: var1, var2, var_n, ...  = machine_learning_model(input_text, params, params, params)
+    # TODO: app.logger.info("/predict model complete...")
+    # TODO: recommendation = machine_learning_model_output....
+    # TODO: return jsonify(recommendation)
 
-    return "So far so good..." # Return basic string to ensure things are working
+    return jsonify("So far so good...") # Return basic string to ensure things are working
 
 if __name__ == "__main__":
     my_app = create_app()
