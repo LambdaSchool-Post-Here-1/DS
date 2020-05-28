2d0adb474f7c83ffe45c900874d5a6c4b22a3431
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-05-28 08:15:28 -0500

    Few minor changes to model training.

--- modeling/train_model.py
+++ modeling/train_model.py
@@ -35,11 +35,11 @@ def model():
 
     # Let's tune the parameters. At the end, we'll send out our parameters.
 
-    # Run 1.
+    # Run 1 - Tune our Vectorizer
     parameters = {
+                  'vect__analyzer': ['word', 'char', 'char_wb'],
                   'vect__ngram_range': [(1, 1), (1, 2), (1, 3),
                                         (1, 4), (1, 5), (1, 6)],
-                  'vect__analyzer': ['word', 'char', 'char_wb'],
                   'vect__norm': ['l1', 'l2']
                   }
 
@@ -51,28 +51,16 @@ def model():
     vect__analyzer = results.best_params_['vect__analyzer']
     vect__norm = results.best_params_['vect__norm']
 
-    # Run 2.
-    parameters = {
-                  'vect__ngram_range': [vect__ngram_range],
-                  'vect__analyzer': [vect__analyzer],
-                  'clf__n_estimators': [25, 50, 100, 125, 150, 175, 200],
-                  'clf__max_features': [None, 'sqrt', 'log2', 25, 50]}
-
-    search = GridSearchCV(pipe, parameters, cv=5, n_jobs=-1, verbose=False)
-    results = search.fit(X_train, y_train)
-
-    clf__n_estimators = results.best_params_['clf__n_estimators']
-    clf__max_features = results.best_params_['clf__max_features']
-
-    # Run 3.
+    # Run 2 - Tune Classification model
     parameters = {
                   'vect__analyzer': [vect__analyzer],
                   'vect__ngram_range': [vect__ngram_range],
-                  'clf__n_estimators': [clf__n_estimators],
-                  'clf__max_features': [clf__max_features],
+                  'vect__norm': [vect__norm],
                   'clf__class_weight': ['balanced', 'balanced_subsample'],
+                  'clf__max_features': [None, 'sqrt', 'log2', 25, 50],
+                  'clf__n_estimators': [25, 50, 100, 125, 150, 175, 200],
                   'clf__oob_score': [True, False]
-                  }
+                 }
 
     search = GridSearchCV(pipe, parameters, cv=5, n_jobs=-1, verbose=False)
     results = search.fit(X_train, y_train)
@@ -81,11 +69,12 @@ def model():
 
     # Now that we've tuned parameters, let's go ahead and build our model!
     vect = TfidfVectorizer(analyzer=params['vect__analyzer'],
-                           ngram_range=params['vect__ngram_range'])
+                           ngram_range=params['vect__ngram_range'],
+                           norm=params['vect__norm'])
 
-    rfc = RandomForestClassifier(n_estimators=params['clf__n_estimators'],
+    rfc = RandomForestClassifier(class_weight=params['clf__class_weight'],
                                  max_features=params['clf__max_features'],
-                                 class_weight=params['clf__class_weight'],
+                                 n_estimators=params['clf__n_estimators'],
                                  oob_score=params['clf__oob_score'])
 
     model = Pipeline([('vect', vect), ('clf', rfc)])
