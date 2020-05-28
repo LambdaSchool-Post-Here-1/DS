110fe93106f6e2f14d3a3aed65d144e3e2c246c2
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-05-28 07:27:23 -0500

    Updates to model training

--- modeling/train_model.py
+++ modeling/train_model.py
@@ -11,96 +11,84 @@ import warnings
 df = pd.read_csv('../data/datasets/cleaned_data.csv')
 
 
-class model():
-    """`tune_parameters` - Tunes our parameters for new data fetches.
-       `nlp_model` - Trains and returns model fit with our parameters"""
+def model():
+    """Tunes our hyperparameters to update model for new data fetches as
+       trends change in devices and such, builds model based upon training,
+       fits it, and returns the model."""
 
-    def __init__(self, training_params, model):
-        self.training_params = training_params
-        self.model = model
+    # 70/30 Train-Test Split
+    train, test = train_test_split(df, train_size=0.7, random_state=42)
 
-    def tune_parameters(self):
-        """Tunes our hyperparameters to update model for new data fetches as
-           trends change in devices and such."""
+    # n_splits warning, fix
+    warnings.simplefilter("ignore", UserWarning)
 
-        train, test = train_test_split(df, train_size=0.7)  # Train-Test Split.
+    X_train = train['Tokens']
+    y_train = train['Subreddit']
 
-        # n_splits warning, fix
-        warnings.simplefilter("ignore", UserWarning)
+    X_test = test['Tokens']
+    y_test = test['Subreddit']
 
-        X_train = train['Tokens']
-        y_train = train['Subreddit']
+    vect = TfidfVectorizer()
+    rfc = RandomForestClassifier()
 
-        X_test = test['Tokens']
-        y_test = test['Subreddit']
+    pipe = Pipeline([('vect', vect), ('clf', rfc)])
 
-        vect = TfidfVectorizer()
-        rfc = RandomForestClassifier()
+    # Let's tune the parameters. At the end, we'll send out our parameters.
 
-        pipe = Pipeline([('vect', vect), ('clf', rfc)])
+    # Run 1.
+    parameters = {
+                  'vect__ngram_range': [(1, 1), (1, 2), (1, 3),
+                                        (1, 4), (1, 5), (1, 6)],
+                  'vect__analyzer': ['word', 'char', 'char_wb'],
+                  'vect__norm': ['l1', 'l2']
+                  }
 
-        # Let's tune the parameters. At the end, we'll send out our parameters.
+    search = GridSearchCV(pipe, parameters, cv=5, n_jobs=-1, verbose=False)
+    results = search.fit(X_train, y_train)
 
-        # Run 1.
-        parameters = {
-                      'vect__ngram_range': [(1, 1), (1, 2), (1, 3),
-                                            (1, 4), (1, 5)],
-                      'vect__analyzer': ['word', 'char', 'char_wb']
-                      }
+    # Best parameters from this run.
+    vect__ngram_range = results.best_params_['vect__ngram_range']
+    vect__analyzer = results.best_params_['vect__analyzer']
+    vect__norm = results.best_params_['vect__norm']
 
-        search = GridSearchCV(pipe, parameters, cv=5, n_jobs=-1, verbose=False)
-        results = search.fit(X_train, y_train)
+    # Run 2.
+    parameters = {
+                  'vect__ngram_range': [vect__ngram_range],
+                  'vect__analyzer': [vect__analyzer],
+                  'clf__n_estimators': [25, 50, 100, 125, 150, 175, 200],
+                  'clf__max_features': [None, 'sqrt', 'log2', 25, 50]}
 
-        # Best parameters from this run.
-        vect__ngram_range = results.best_params_['vect__ngram_range']
-        vect__analyzer = results.best_params_['vect__analyzer']
+    search = GridSearchCV(pipe, parameters, cv=5, n_jobs=-1, verbose=False)
+    results = search.fit(X_train, y_train)
 
-        # Run 2.
-        parameters = {
-                      'vect__ngram_range': [vect__ngram_range],
-                      'vect__analyzer': [vect__analyzer],
-                      'clf__n_estimators': [25, 50, 100, 125, 150, 175, 200],
-                      'clf__max_features': [None, 'sqrt', 'log2'],
-                      'clf__max_samples': [None, 25, 35, 50, 65, 70],
-                      'clf__class_weight': ['balanced', 'balanced_subsample']
-                     }
+    clf__n_estimators = results.best_params_['clf__n_estimators']
+    clf__max_features = results.best_params_['clf__max_features']
 
-        search = GridSearchCV(pipe, parameters, cv=5, n_jobs=-1, verbose=False)
-        results = search.fit(X_train, y_train)
+    # Run 3.
+    parameters = {
+                  'vect__analyzer': [vect__analyzer],
+                  'vect__ngram_range': [vect__ngram_range],
+                  'clf__n_estimators': [clf__n_estimators],
+                  'clf__max_features': [clf__max_features],
+                  'clf__class_weight': ['balanced', 'balanced_subsample'],
+                  'clf__oob_score': [True, False]
+                  }
 
-        clf__n_estimators = results.best_params_['clf__n_estimators']
-        clf__max_features = results.best_params_['clf__max_features']
-        clf__max_samples = results.best_params_['clf__max_samples']
+    search = GridSearchCV(pipe, parameters, cv=5, n_jobs=-1, verbose=False)
+    results = search.fit(X_train, y_train)
 
-        # Run 3.
-        parameters = {
-                      'vect__analyzer': [vect__analyzer],
-                      'vect__ngram_range': [vect__ngram_range],
-                      'clf__n_estimators': [clf__n_estimators],
-                      'clf__max_features': [clf__max_features],
-                      'clf__class_weight': ['balanced', 'balanced_subsample'],
-                      'clf__oob_score': [True, False]
-                      }
+    params = results.best_params_
 
-        search = GridSearchCV(pipe, parameters, cv=5, n_jobs=-1, verbose=False)
-        results = search.fit(X_train, y_train)
+    # Now that we've tuned parameters, let's go ahead and build our model!
+    vect = TfidfVectorizer(analyzer=params['vect__analyzer'],
+                           ngram_range=params['vect__ngram_range'])
 
-        training_params.self = results.best_params_
-        return training_params
+    rfc = RandomForestClassifier(n_estimators=params['clf__n_estimators'],
+                                 max_features=params['clf__max_features'],
+                                 class_weight=params['clf__class_weight'],
+                                 oob_score=params['clf__oob_score'])
 
-    def nlp_model(self, training_params):
-        """Trains model for use in making predictions on user input"""
+    model = Pipeline([('vect', vect), ('clf', rfc)])
+    model.fit(df['Tokens'], df['Subreddit'])
 
-        params = training_params.self
-        vect = TfidfVectorizer(analyzer=params['vect__analyzer'],
-                               ngram_range=params['vect__ngram_range'])
-
-        rfc = RandomForestClassifier(n_estimators=params['clf__n_estimators'],
-                                     max_features=params['clf__max_features'],
-                                     class_weight=params['clf__class_weight'],
-                                     oob_score=params['clf__oob_score'])
-
-        model = Pipeline([('vect', vect), ('clf', rfc)])
-        model.fit(df['Tokens'], df['Subreddit'])
-
-        return model
+    return model
