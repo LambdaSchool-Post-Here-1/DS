b6ee3ca0455cb6a58c33e3976faa66064e1a9e3e
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-05-28 08:23:29 -0500

    Update in accordance with updates in

--- modeling/predict.py
+++ modeling/predict.py
@@ -1,17 +1,15 @@
 """Makes predictions using trained model."""
 
-from train_model import nlp_model
+from train_model import model
 from data.clean_preprocess import tokenize
 
 
-model = nlp_model()
+model = model()
 
 
 def make_prediction(user_input):
     """Tokenizes texts using custom tokenizer, and makes predictions based upon
        model as trained in train_model.py"""
-
-    model = model.self
 
     tokenized_text = tokenize(user_input)
     prediction = model.predict(tokenized_text)
