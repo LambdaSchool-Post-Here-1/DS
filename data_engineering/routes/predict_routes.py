36bca9cab973ecc7c71463be9eedbac98cace283
Poenomaly <djbenson741@gmail.com>
2020-05-28 14:49:48 -0600

    Bug fixes

--- data_engineering/routes/predict_routes.py
+++ data_engineering/routes/predict_routes.py
@@ -22,22 +22,3 @@ def predict():
 
 
     return jsonify(prediction[0]) # Return basic jsonified string to ensure things are working
-
-    # def predict(): #define a prediction function
-    #     # Get input text from request body
-    #     body = request.get_json(force=True)
-    #     text = body['text']
-    #     # Run through model
-    #     knnPickle = 'api/modelknn_pkl.sav'
-    #     model, index_df, tfidf = pickle.load(open(knnPickle, 'rb'))
-    #     dist, indices = model.kneighbors(tfidf.transform([text]).todense())
-    #     recommended_subreddits = [index_df.iloc[n]['subreddit'] for n in indices[0]]
-    #     resp = {
-    #         'subreddits': uniq(recommended_subreddits)[:3]
-    #     }
-    #     # give output to sender.
-    #     return APP.response_class(
-    #         response=json.dumps(resp),
-    #         status=200,
-    #         mimetype='application/json'
-    #     ) (edited) 
