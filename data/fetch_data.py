28d5a9612c17be8468c9ea7a16554c1b5f3a77b1
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-05-28 08:16:19 -0500

    Changed default number of posts to fetch

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -7,9 +7,9 @@ import praw  # Reddit API wrapper
 from decouple import config
 
 
-def fetch(num_posts=250):
+def fetch(num_posts=200):
     """Fetches the top posts for our list of technical support subreddits.
-       `num_posts` (Default = 250) # of posts to fetch from each subreddit."""
+       `num_posts` (Default = 200) # of posts to fetch from each subreddit."""
 
     # Make connection to Reddit API
     r = praw.Reddit(client_id=config('CLIENT_ID'),
