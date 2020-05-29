b3de543cb49e0bbf97c428ee4bed44098768cb08
Poenomaly <djbenson741@gmail.com>
2020-05-28 19:35:44 -0600

    Bug fixes

--- modeling/train_model.py
+++ modeling/train_model.py
@@ -2,6 +2,7 @@
 
 
 import pandas as pd
+import sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
